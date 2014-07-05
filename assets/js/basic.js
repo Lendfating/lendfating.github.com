b8402786034c3b41560b8a912b07179b9bdd0de7
lendfating <lizhen19900409@126.com>
2014-07-06 00:11:30 +0800

    23

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -68,26 +68,46 @@ var setting = {
 		setting.obj_box = jQuery(".navbar-setting .setting-box");
 		setting.mode = jQuery(".navbar-setting a.btn.btn-daytime.active");
 		setting.font = jQuery(".navbar-setting a.btn.font.active");
+		// 根据 cookie 记录，修改初始显示方式
+		if (setting.getCookie("mode")=="reader-night-mode"){
+			setting.setMode(jQuery(".navbar-setting a.btn.btn-nighttime"));
+		}
+		if (setting.getCookie("font")=="reader-font2"){
+			setting.setFont(jQuery(".navbar-setting a.btn.font.hei"));
+		}
+		// 设置响应事件
 		setting.obj_bt.click(setting.open);	
 		setting.obj_box.click(function(){return false;});
 		jQuery(".navbar-setting .change-background a.btn").click(function(){
-			// 背景颜色设置
-			jQuery("body").removeClass(setting.mode.attr("data"));
-			jQuery("body").addClass($(this).attr("data"));
-			setting.mode.removeClass("active");
-			$(this).addClass("active");
-			setting.mode = $(this);
+			// 根据用户的操作，更改背景颜色设置
+			setting.setMode($(this));
 			setting.close();
 		});
 		jQuery(".navbar-setting .change-font a.btn").click(function(){
-			// 字体设置
-			jQuery("body").removeClass(setting.font.attr("data"));
-			jQuery("body").addClass($(this).attr("data"));
-			setting.font.removeClass("active");
-			$(this).addClass("active");
-			setting.font = $(this);
+			// 根据用户的操作，更改字体设置
+			setting.setFont($(this));
 			setting.close();
 		});
+	},
+	setMode: function(mode){
+		// 背景颜色设置
+		jQuery("body").removeClass(setting.mode.attr("data"));
+		jQuery("body").addClass(mode.attr("data"));
+		setting.mode.removeClass("active");
+		mode.addClass("active");
+		setting.mode = mode;
+		// 记录该模式
+		setting.setCookie("mode", $(this).attr("data"), 30);
+	},
+	setFont: function(font){
+		// 字体设置
+		jQuery("body").removeClass(setting.font.attr("data"));
+		jQuery("body").addClass(font.attr("data"));
+		setting.font.removeClass("active");
+		font.addClass("active");
+		setting.font = font;
+		// 记录该字体
+		setting.setCookie("font", $(this).attr("data"), 30);
 	},
 	open: function(){
 		if (menu.opened) menu.close();
@@ -101,6 +121,21 @@ var setting = {
 		setting.obj_box.removeClass("box-open");	// setting-box
 		jQuery(window).click(function(){});
 		setting.opened = false;
+	},
+	getCookie: function(cname){
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i].trim();
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+		return "";
+	},
+	setCookie: function(cname, cvalue, exdays){
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toGMTString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
 	}
 };
 
